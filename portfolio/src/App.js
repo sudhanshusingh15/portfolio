2934931e270fafc081cd864db37060541488f51f
sudhanshusingh <ss4827@drexel.edu>
2024-02-20 06:32:57 -0500

    added name

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { GridComponent } from './containers/gridComponent/index.jsx';
+import { TopSection } from './pages/homePage/topSection.jsx';
 
 function App() {
-  return <GridComponent />;
+  useEffect(() => {
+    document.title = 'Portfolio';
+  },[]);
+
+
+  return <TopSection />;
 }
 
 export default App;
---
+++ portfolio/src/pages/homePage/topSection.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import styled, { css }from "styled-components";
+
+
+const TopSectionContainer = styled.div`
+    width: 100%;
+    height: 200vh;
+    background-color: #000;
+`;
+
+
+const TextContainer = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    border: 1px solid red;
+    padding: 0;
+    ${props => css`
+        width: ${props.width || "100%"};
+        height: ${props.height || 'auto'};
+    `}
+`;
+
+const Title = styled.h1`
+    font-family: acorn, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    letter-spacing: -4px;
+    font-feature-settings: "liga";
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    color: white;
+    justify-content: space-evenly;
+    word-break: break-word;
+    text-align: center;
+    margin-bottom: 0;
+
+    ${props => props.firstTitle && css`
+        margin-bottom: -1.2em;
+    `}
+
+    ${props => css`
+        font-size: ${props.fontSize || "160px"};
+        font-weight: ${props.fontWeight || "700"};
+        color: ${props.color || "white"};
+    `}
+`;
+
+const Text = styled.p`
+    font-family: acorn, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    letter-spacing: -0.3px;
+    font-feature-settings: "liga";
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    color: white;     
+    justify-content: space-evenly;
+    word-break: break-word;
+    text-align: center;
+    margin-bottom: 0;
+
+    ${props => css`
+        font-size: ${props.fontSize || "30px"};
+        font-weight: ${props.fontWeight || "700"};
+        color: ${props.color || "white"};
+    `}
+`;
+
+export function TopSection(props) {
+    return (
+        <TopSectionContainer>
+            <TextContainer height="100vh">
+                <Title firstTitle>Hello! I am</Title>
+                <Title>Sudhanshu Singh</Title>
+                <Text fontSize="30px" fontWeight="0">Software Engineer</Text>
+            </TextContainer>
+        </TopSectionContainer>
+    );
+}
