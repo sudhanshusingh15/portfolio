7b5802b5ca5bce3e1ab95dd8c9404dc655526b73
sudhanshusingh <ss4827@drexel.edu>
2024-02-26 07:21:15 -0500

    updated topsection css

--- portfolio/src/pages/homePage/topSection.jsx
+++ portfolio/src/pages/homePage/topSection.jsx
@@ -53,13 +53,13 @@ const Title = styled.h1`
     text-align: center;
     margin: 0; 
     padding: 0; 
-
+    opacity: 0;
     ${props => props.isTitleVisible && css`
-        animation: ${slideDown} 1.5s ease forwards;
+        animation: ${slideDown} 1.5s ease forwards, fadeIn 0.5s ease forwards;
     `}
    
     ${props => props.firstTitle && css`
-        margin-top: 0.45em;
+        margin-top: 0.40em;
         margin-bottom: -0.35em;
     `}
 
@@ -74,14 +74,23 @@ const Title = styled.h1`
 // -webkit-text-stroke-color: red;
 
 const Text = styled.p`
-    font-family: acorn, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    font-family: "FuturaCnd", "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
     letter-spacing: -0.3px;
     font-feature-settings: "liga";
     text-rendering: optimizeLegibility;
     -webkit-font-smoothing: antialiased;    
     text-align: center;
-    margin: 0; 
-    padding: 0;
+    margin: 0;
+    padding: 5px;
+    opacity: 0;
+    ${props => props.isTextVisible && css`
+        animation: ${slideDown} 1.5s ease forwards;
+    `}
+
+    ${props => props.firstText && css`
+        margin-top: 0.7em;
+    `}
+
     ${props => css`
         font-size: ${props.fontSize || "30px"};
         font-weight: ${props.fontWeight || "700"};
@@ -91,14 +100,25 @@ const Text = styled.p`
 
 export function TopSection(props) {
     const [isTitleVisible, setIsTitleVisible] = useState(false);
+    const [isTextVisible, setIsTextVisible] = useState(false);
 
     useEffect(() => {
-        const timer = setTimeout(() => {
+        const titleTimer = setTimeout(() => {
             setIsTitleVisible(true);
-        }, 500); // Delay of 500ms
+        }, 500);
 
-        return () => clearTimeout(timer);
+        return () => clearTimeout(titleTimer);
     }, []);
+
+    useEffect(() => {
+        if (isTitleVisible) {
+            const textTimer = setTimeout(() => {
+                setIsTextVisible(true);
+            }, 1000);
+
+            return () => clearTimeout(textTimer);
+        }
+    }, [isTitleVisible]);
 
     return (
         <TopSectionContainer>
@@ -106,7 +126,8 @@ export function TopSection(props) {
             <TextContainer height="auto">
                 <Title firstTitle isTitleVisible={isTitleVisible}>Hello! I am</Title>
                 <Title isTitleVisible={isTitleVisible}>Sudhanshu</Title>
-                <Text fontSize="30px" fontWeight="0">Software Engineer</Text>
+                <Text firstText fontSize="22px" fontWeight="0" isTextVisible={isTextVisible}>I am passionate about merging technology and user-centric design</Text>
+                <Text fontSize="22px" fontWeight="0" isTextVisible={isTextVisible}>to craft seamless and intuitive digital experiences</Text>
             </TextContainer>
         </TopSectionContainer>
     );
